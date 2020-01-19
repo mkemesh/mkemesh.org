bf1d4c7e969361257390ecc8148d09bea2d24f00
JonH <>
2020-01-18 23:37:50 -0600

    Primitive Contact form.

--- js/client.js
+++ js/client.js
@@ -1,18 +1,27 @@
+/// <input> has automatic event handlers. Get instance and set Event Handler.
+const button = document.querySelector('#send');
+button.addEventListener('click', sendRequest(
+	document.querySelector('#name').value,
+	document.querySelector('#email').value,
+	document.querySelector('#message').value
+));
+
 function sendRequest(method, endPoint, body, callback) {
 	var http = new XMLHttpRequest();
 	http.open(method, endPoint);
-	http.send(body);	
+	http.send(body);
 	http.onreadystatechange = e => {
 		callback(e);
 	};
 }
 
+/// Take all the text and send it to the server.
 function sendContactEMail(name, email, message, callback) {
 	var mailEndpoint = "https://api.mkemesh.org/v1/contact-email";
 	var payload = {
 		name: name,
 		email: email,
 		message: message
-	};	
+	};
 	this.sendRequest("POST", mailEndpoint, JSON.stringify(payload), callback);
 }
